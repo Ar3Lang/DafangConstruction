09ecc5f837b16505208f48eacee04ac4adb1264f
Ar3Lang <wheazewolf@gmail.com>
2026-02-01 19:51:10 +0800

    The First Submit
    Signed-off-by: Ar3Lang <wheazewolf@gmail.com>

---
+++ README.md
@@ -0,0 +1,1 @@
+Blockus is a mod for Minecraft 1.20 that adds blocks!
---
+++ src/main/java/com/ar3lang/dafangconstruction/DafangConstructionDataGenerator.java
@@ -0,0 +1,11 @@
+package com.ar3lang.dafangconstruction;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DafangConstructionDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
